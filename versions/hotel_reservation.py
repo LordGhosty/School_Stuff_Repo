897d2eba95e8dfb42d93f2133e5a14393371e6ed
Lord Ghosty <mariosbots@gmail.com>
2026-05-06 11:14:53 +0300

    Add update reservation functionality to hotel reservation system

--- versions/hotel_reservation.py
+++ versions/hotel_reservation.py
@@ -12,4 +12,12 @@ def book_room(name, room_number):
     reservations[room_number] = name
     print(f' Κράτηση επιβεβαιώθηκε για {name} στο δωμάτιο {room_number}')
 
+def update_reservation(room_number, new_name):
+    if room_number not in reservations:
+        raise KeyError(' Το δωμάτιο δεν έχει κράτηση!')
+    reservations[room_number] = new_name
+    print(f' Το δωμάτιο {room_number} ανατέθηκε στον {new_name}')
+
+
+
 view_reservations()
